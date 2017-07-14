71778f63badc35127066d321a0a7718fafa2cc99
Carolyn Harris <ccharris27@gmail.com>
2017-07-14 13:53:08 -0400

    Delete config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    applicationId: 'fcf32339f5c50b1d7523e61de64918324a074492906171eec2fa74e573237e41',
-    secret: 'bcfca4fac61d53a9eeb3257e3666880b3a89d7bb40fc8e5620c71fc799e8786b',
-    callbackUrl: 'urn:ietf:wg:oauth:2.0:oob',
-    Authorization: 'Client-ID fcf32339f5c50b1d7523e61de64918324a074492906171eec2fa74e573237e41'
-}
-
